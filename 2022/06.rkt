fc2dbbcb82b214309d45528b0037fab6f6e7a9e0
Lucas Sta Maria <lucas@priime.dev>
2023-07-08 17:21:56 -0700

    feat: add 2022 day 6
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ 2022/06.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require threading)
+
+#; {[Listof Char] Natural -> Natural}
+(define (find-start s n)
+  (for/fold ([done? #f]
+             [idx n]
+             [chars s]
+             #:result idx)
+            ([_ (in-naturals)]
+             #:break done?)
+    (cond [(= (set-count (list->set (take chars n))) n)
+           (values #t idx #f)]
+          [else
+           (values #f (add1 idx) (cdr chars))])))
+
+(call-with-input-file "06.txt"
+  (lambda (in)
+    (define data (port->string in))
+
+    ;; Part 1
+    (println (find-start (string->list data) 4))
+    ;; Part 2
+    (println (find-start (string->list data) 14))))
